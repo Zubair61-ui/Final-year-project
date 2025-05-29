610a49f4c4c03c66448c709ca68b48cd9ab81682
Zubair Hashmat <111867413+Zubair61-ui@users.noreply.github.com>
2025-05-30 00:41:38 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Provider } from "@/components/Providers";
 import { Toaster } from "@/components/ui/toaster";
 
 const lexend = Lexend({ subsets: ["latin"] });
-export const runtime = "edge";
+
 
 
 export const metadata: Metadata = {
